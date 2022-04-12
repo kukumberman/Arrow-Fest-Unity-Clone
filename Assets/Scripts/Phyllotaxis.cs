a2c11b21b80a4a3dbbf543c97056356330ff46c6
Dima <strongbaby1337@gmail.com>
2022-04-12 23:24:22 +0300

    Update Phyllotaxis.cs

--- Assets/Scripts/Phyllotaxis.cs
+++ Assets/Scripts/Phyllotaxis.cs
@@ -6,16 +6,19 @@ public class Phyllotaxis : MonoBehaviour
 {
     [SerializeField] private float m_Angle = 137.5f;
     [SerializeField] private float m_Scale = 1;
+    [SerializeField] private Vector3 m_Normal = Vector3.forward;
 
-    public Vector2 Calculate(int index)
+    public Vector3 Calculate(int index)
     {
         float angle = index * (m_Angle * Mathf.Deg2Rad);
         float scale = m_Scale / 10;
         float radius = scale * Mathf.Sqrt(index);
 
-        Vector2 pos = Vector2.zero;
+        Vector3 pos = Vector3.zero;
         pos.x = Mathf.Sin(angle) * radius;
         pos.y = Mathf.Cos(angle) * radius;
-        return pos;
+
+        Quaternion rotation = Quaternion.FromToRotation(Vector3.forward, m_Normal);
+        return rotation * pos;
     }
 }
